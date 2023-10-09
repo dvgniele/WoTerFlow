a78efd9286991f4466dec84ad0f92ad8c6778bf6
Daniele Perrella <ktm17@live.it>
2023-10-09 23:36:08 +0200

    rft: moved utils to companion object

--- kt_app/src/main/kotlin/utils/Utils.kt
+++ kt_app/src/main/kotlin/utils/Utils.kt
@@ -22,131 +22,134 @@ import java.io.InputStreamReader
 import java.net.URL
 
 class Utils {
-    val jsonMapper: ObjectMapper = jacksonObjectMapper()
+    companion object {
+        val jsonMapper: ObjectMapper = jacksonObjectMapper()
 
-    fun downloadFileAsString(uri: String): String {
-        val url = URL(uri)
-        val connection = url.openConnection()
-        val reader = BufferedReader(InputStreamReader(connection.getInputStream()))
-        val content = StringBuilder()
-        var line: String?
+        fun downloadFileAsString(uri: String): String {
+            val url = URL(uri)
+            val connection = url.openConnection()
+            val reader = BufferedReader(InputStreamReader(connection.getInputStream()))
+            val content = StringBuilder()
+            var line: String?
 
-        try {
-            while (reader.readLine().also { line = it } != null) {
-                content.append(line).append("\n")
+            try {
+                while (reader.readLine().also { line = it } != null) {
+                    content.append(line).append("\n")
+                }
+            } finally {
+                reader.close()
             }
-        } finally {
-            reader.close()
+
+            return content.toString()
         }
 
-        return content.toString()
-    }
-
-    fun readFileAsStream(filePath: String): InputStream? {
-        return try {
-            File(filePath).inputStream()
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-    fun loadModel(filePath: String, lang: Lang): Model {
-        try {
-            val inputStream = readFileAsStream(filePath)
-
-            if (inputStream != null) {
-                val model = ModelFactory.createDefaultModel()
-                RDFDataMgr.read(model, inputStream, lang)
-
-                //println("Model loaded successfully\n${model.toString()}")
-                return model
-            } else {
-                throw ThingException("Resource content is null for path: $filePath")
+        fun readFileAsStream(filePath: String): InputStream? {
+            return try {
+                File(filePath).inputStream()
+            } catch (e: Exception) {
+                null
             }
-        } catch (e: Exception) {
-            println("\n\nException: $e. path: $filePath")
-            throw ThingException("Cannot load model: ${e.message} path: $filePath")
-        }
-    }
-
-    fun loadRDFDatasetIntoModelList(dataset: Dataset): ArrayList<Model> {
-        val modelList = ArrayList<Model>()
-
-        dataset.listNames().forEach { graphName ->
-            val model = dataset.getNamedModel(graphName)
-            modelList.add(model)
         }
 
-        return modelList
-    }
+        fun loadModel(filePath: String, lang: Lang): Model {
+            try {
+                val inputStream = readFileAsStream(filePath)
 
-    fun loadRDFModelById(dataset: Dataset, graphId: String): Model {
-        return dataset.getNamedModel(graphId)
-    }
+                if (inputStream != null) {
+                    val model = ModelFactory.createDefaultModel()
+                    RDFDataMgr.read(model, inputStream, lang)
 
-    fun hasThingType(thing: ObjectNode): Boolean {
-        var type = thing["@type"]
-
-        if (type is JsonNode) {
-            val typeValue = type.asText()
-            return typeValue == "Thing" || typeValue == "https://www.w3.org/2019/wot/td#Thing"
-        } else if (type is ArrayNode) {
-            val uniqueTypes = type.map { it.asText() }.toSet()
-            return uniqueTypes.any { it == "Thing" || it == "https://www.w3.org/2019/wot/td#Thing" }
+                    //println("Model loaded successfully\n${model.toString()}")
+                    return model
+                } else {
+                    throw ThingException("Resource content is null for path: $filePath")
+                }
+            } catch (e: Exception) {
+                println("\n\nException: $e. path: $filePath")
+                throw ThingException("Cannot load model: ${e.message} path: $filePath")
+            }
         }
 
-        return false
-    }
+        fun loadRDFDatasetIntoModelList(dataset: Dataset): ArrayList<Model> {
+            val modelList = ArrayList<Model>()
 
-    fun strconcat(vararg values: String): String {
-        return values.joinToString("")
-    }
+            dataset.listNames().forEach { graphName ->
+                val model = dataset.getNamedModel(graphName)
+                modelList.add(model)
+            }
 
-    fun hasValidId(id: String?): String {
-        if (id.isNullOrEmpty()) {
-            throw ThingException("Thing ID not valid.")
+            return modelList
         }
-        return id
-    }
 
-    fun hasJsonContent(contentTypeHeader: String?): Boolean {
-        if (contentTypeHeader == null)
+        fun loadRDFModelById(dataset: Dataset, graphId: String): Model {
+            return dataset.getNamedModel(graphId)
+        }
+
+        fun hasThingType(thing: ObjectNode): Boolean {
+            val type = thing["@type"]
+
+            if (type is JsonNode) {
+                val typeValue = type.asText()
+                return typeValue == "Thing" || typeValue == "https://www.w3.org/2019/wot/td#Thing"
+            } else if (type is ArrayNode) {
+                val uniqueTypes = type.map { it.asText() }.toSet()
+                return uniqueTypes.any { it == "Thing" || it == "https://www.w3.org/2019/wot/td#Thing" }
+            }
+
             return false
-
-        val contentType = ContentType.parse(contentTypeHeader)
-        val allowedContentTypes = listOf(
-            ContentType.Application.Json,
-            ContentType.parse("application/td+json"),
-            ContentType.parse("application/ld+json"),
-            ContentType.parse("application/merge-patch+json")
-        )
-
-        return allowedContentTypes.any { it.match(contentType) }
-    }
-
-    fun hasBody(body: String?): ObjectNode? {
-        if (body.isNullOrEmpty()) {
-            throw ThingException("The request body is empty.")
         }
 
-        return jsonMapper.readValue(body)
-    }
+        fun strconcat(vararg values: String): String {
+            return values.joinToString("")
+        }
 
-    fun idExists(map: MutableSet<String>, id: String): Boolean {
-        return map.contains(id)
-    }
+        fun hasValidId(id: String?): String {
+            if (id.isNullOrEmpty()) {
+                throw ThingException("Thing ID not valid.")
+            }
+            return id
+        }
 
-    fun contextsContains(contexts: ArrayNode, targetContext: String): Boolean {
-        return contexts.any { it.isTextual && it.asText() == targetContext }
-    }
+        fun hasJsonContent(contentTypeHeader: String?): Boolean {
+            if (contentTypeHeader == null)
+                return false
 
-    fun isJsonLd11OrGreater(thing: ObjectNode): Boolean {
-        val versionNode = thing["@version"]
-        return versionNode != null
-    }
+            val contentType = ContentType.parse(contentTypeHeader)
+            val allowedContentTypes = listOf(
+                ContentType.Application.Json,
+                ContentType.parse("application/td+json"),
+                ContentType.parse("application/ld+json"),
+                ContentType.parse("application/merge-patch+json")
+            )
 
-    fun toJson(td: String): ObjectNode {
-        val objectMapper = ObjectMapper()
-        return objectMapper.readValue<ObjectNode>(td)
+            return allowedContentTypes.any { it.match(contentType) }
+        }
+
+        fun hasBody(body: String?): ObjectNode? {
+            if (body.isNullOrEmpty()) {
+                throw ThingException("The request body is empty.")
+            }
+
+            return jsonMapper.readValue(body)
+        }
+
+        fun idExists(map: MutableSet<String>, id: String): Boolean {
+            return map.contains(id)
+        }
+
+        fun contextsContains(contexts: ArrayNode, targetContext: String): Boolean {
+            return contexts.any { it.isTextual && it.asText() == targetContext }
+        }
+
+        fun isJsonLd11OrGreater(thing: ObjectNode): Boolean {
+            val versionNode = thing["@version"]
+            return versionNode != null
+        }
+
+        fun toJson(td: String): ObjectNode {
+            val objectMapper = ObjectMapper()
+            return objectMapper.readValue<ObjectNode>(td)
+
+        }
     }
 }
