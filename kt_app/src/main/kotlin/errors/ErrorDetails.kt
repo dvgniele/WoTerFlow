e8735cb152ca70eefe8e4c341c1014b8bbc83e64
dvgniele <ktm17@live.it>
2023-11-17 01:12:48 +0100

    add: doc for errors

--- kt_app/src/main/kotlin/errors/ErrorDetails.kt
+++ kt_app/src/main/kotlin/errors/ErrorDetails.kt
@@ -1,5 +1,15 @@
 package errors
 
+/**
+ * Represents details of an error response.
+ *
+ * @property type The type of error, if available.
+ * @property title The title of the error.
+ * @property status The HTTP Status Code associated with the error.
+ * @property detail A detailed description of the errors.
+ * @property instance The specific instance or occurrence of the errors, if available.
+ * @property validationErrors A list of the validation errors, if applicable.
+ */
 data class ErrorDetails (
     val type: String? = null,
     val title: String,
