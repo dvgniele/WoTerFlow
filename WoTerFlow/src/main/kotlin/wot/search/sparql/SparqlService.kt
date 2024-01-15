8a429c892de2e9b17901d20a960034c1a65d8ef0
dvgniele <ktm17@live.it>
2024-01-15 18:28:31 +0100

    rft: major code quality improvement

--- WoTerFlow/src/main/kotlin/wot/search/sparql/SparqlService.kt
+++ WoTerFlow/src/main/kotlin/wot/search/sparql/SparqlService.kt
@@ -3,6 +3,7 @@ package wot.search.sparql
 import com.jayway.jsonpath.JsonPath
 import exceptions.UnsupportedSparqlQueryException
 import org.apache.jena.query.Dataset
+import org.apache.jena.query.QueryExecution
 import org.apache.jena.query.TxnType
 import org.apache.jena.query.QueryExecutionFactory
 import org.apache.jena.query.QueryFactory
@@ -29,84 +30,24 @@ class SparqlService {
          * @return [ByteArrayOutputStream] containing the query results.
          */
         fun executeQuery(query: String, format: ResultsFormat, dataset: Dataset): ByteArrayOutputStream {
-            dataset.begin(TxnType.WRITE)
+            return dataset.writeTransaction {
+                val outputStream = ByteArrayOutputStream()
+                val queryExecution = QueryExecutionFactory.create(QueryFactory.create(query, Syntax.syntaxSPARQL_11), dataset)
 
-            val outputStream = ByteArrayOutputStream()
-            val queryExecution = QueryExecutionFactory.create(QueryFactory.create(query, Syntax.syntaxSPARQL_11), dataset)
-
-            try {
-                when {
-                    queryExecution.query.isAskType -> {
-                        when (format) {
-                            ResultsFormat.FMT_RS_JSON -> ResultSetFormatter.outputAsJSON(
-                                outputStream,
-                                queryExecution.execAsk()
-                            )
-
-                            ResultsFormat.FMT_RS_XML -> ResultSetFormatter.outputAsXML(
-                                outputStream,
-                                queryExecution.execAsk()
-                            )
-
-                            ResultsFormat.FMT_RS_CSV -> ResultSetFormatter.outputAsCSV(
-                                outputStream,
-                                queryExecution.execAsk()
-                            )
-
-                            ResultsFormat.FMT_RS_TSV -> ResultSetFormatter.outputAsTSV(
-                                outputStream,
-                                queryExecution.execAsk()
-                            )
-
-                            else -> throw UnsupportedSparqlQueryException("Unsupported format for ASK query")
-                        }
+                try {
+                    when {
+                        queryExecution.query.isAskType -> executeAskQuery(queryExecution, format, outputStream)
+                        queryExecution.query.isSelectType -> executeSelectQuery(queryExecution, format, outputStream)
+                        queryExecution.query.isConstructType || queryExecution.query.isDescribeType ->
+                            executeConstructOrDescribeQuery(queryExecution, outputStream)
+                        else -> throw UnsupportedSparqlQueryException("Unsupported query type")
                     }
-
-                    queryExecution.query.isSelectType -> {
-                        when (format) {
-                            ResultsFormat.FMT_RS_JSON -> ResultSetFormatter.outputAsJSON(
-                                outputStream,
-                                queryExecution.execSelect()
-                            )
-
-                            ResultsFormat.FMT_RS_XML -> ResultSetFormatter.outputAsXML(
-                                outputStream,
-                                queryExecution.execSelect()
-                            )
-
-                            ResultsFormat.FMT_RS_CSV -> ResultSetFormatter.outputAsCSV(
-                                outputStream,
-                                queryExecution.execSelect()
-                            )
-
-                            ResultsFormat.FMT_RS_TSV -> ResultSetFormatter.outputAsTSV(
-                                outputStream,
-                                queryExecution.execSelect()
-                            )
-
-                            else -> throw UnsupportedSparqlQueryException("Unsupported format for SELECT query")
-                        }
-                    }
-
-                    queryExecution.query.isConstructType || queryExecution.query.isDescribeType -> {
-                        val resultModel = if (queryExecution.query.isConstructType) {
-                            queryExecution.execConstruct()
-                        } else {
-                            queryExecution.execDescribe()
-                        }
-
-                        resultModel.write(outputStream, Lang.TURTLE.name)
-                    }
-                    else -> throw UnsupportedSparqlQueryException("Unsupported query type")
+                }finally {
+                    queryExecution.close()
                 }
 
-                dataset.commit()
-            } finally {
-                queryExecution.close()
-                dataset.end()
+                outputStream
             }
-
-            return outputStream
         }
 
         /**
@@ -124,6 +65,74 @@ class SparqlService {
             } catch (e: Exception) {
                 throw Exception("${e.message}")
             }
+        }
+
+        /**
+         * Executes the provided [action] within a write transaction on the current [Dataset].
+         * Right after executing the action, the transaction is committed and closed.
+         *
+         * @param action The [action] to be executed within the write transaction.
+         * @return The result of the [action], typically a [ByteArrayOutputStream]
+         */
+        private fun Dataset.writeTransaction(action: Dataset.(Dataset) -> ByteArrayOutputStream): ByteArrayOutputStream {
+            begin(TxnType.WRITE)
+            try {
+                return action(this, this)
+            } finally {
+                commit()
+                end()
+            }
+        }
+
+        /**
+         * Executes a SPARQL ASK query and formats the results based on the specified [format].
+         *
+         * @param queryExecution The [QueryExecution] object for the ASK query.
+         * @param format The desired [ResultsFormat] for the results (JSON, XML, CSV, TSV).
+         * @param outputStream The output stream to which the formatted results are written.
+         * @throws UnsupportedSparqlQueryException if the specified format is not supported for ASK queries.
+         */
+        private fun executeAskQuery(queryExecution: QueryExecution, format: ResultsFormat, outputStream: ByteArrayOutputStream) {
+            when (format) {
+                ResultsFormat.FMT_RS_JSON -> ResultSetFormatter.outputAsJSON(outputStream, queryExecution.execAsk())
+                ResultsFormat.FMT_RS_XML -> ResultSetFormatter.outputAsXML(outputStream, queryExecution.execAsk())
+                ResultsFormat.FMT_RS_CSV -> ResultSetFormatter.outputAsCSV(outputStream, queryExecution.execAsk())
+                ResultsFormat.FMT_RS_TSV -> ResultSetFormatter.outputAsTSV(outputStream, queryExecution.execAsk())
+                else -> throw UnsupportedSparqlQueryException("Unsupported format for ASK query")
+            }
+        }
+
+        /**
+         * Executes a SPARQL SELECT query and formats the results based on the specified [format].
+         *
+         * @param queryExecution The [QueryExecution] object for the SELECT query.
+         * @param format The desired [ResultsFormat] for the results (JSON, XML, CSV, TSV).
+         * @param outputStream The output stream to which the formatted results are written.
+         * @throws UnsupportedSparqlQueryException if the specified format is not supported for SELECT queries.
+         */
+        private fun executeSelectQuery(queryExecution: QueryExecution, format: ResultsFormat, outputStream: ByteArrayOutputStream) {
+            when (format) {
+                ResultsFormat.FMT_RS_JSON -> ResultSetFormatter.outputAsJSON(outputStream, queryExecution.execSelect())
+                ResultsFormat.FMT_RS_XML -> ResultSetFormatter.outputAsXML(outputStream, queryExecution.execSelect())
+                ResultsFormat.FMT_RS_CSV -> ResultSetFormatter.outputAsCSV(outputStream, queryExecution.execSelect())
+                ResultsFormat.FMT_RS_TSV -> ResultSetFormatter.outputAsTSV(outputStream, queryExecution.execSelect())
+                else -> throw UnsupportedSparqlQueryException("Unsupported format for SELECT query")
+            }
+        }
+
+        /**
+         * Executes a SPARQL CONSTRUCT or DESCRIBE query and writes the results to the specified [outputStream].
+         *
+         * @param queryExecution The [QueryExecution] object for the CONSTRUCT or DESCRIBE query.
+         * @param outputStream The output stream to which the results are written.
+         */
+        private fun executeConstructOrDescribeQuery(queryExecution: QueryExecution, outputStream: ByteArrayOutputStream) {
+            val resultModel = if (queryExecution.query.isConstructType) {
+                queryExecution.execConstruct()
+            } else {
+                queryExecution.execDescribe()
+            }
+            resultModel.write(outputStream, Lang.TURTLE.name)
         }
     }
 }
